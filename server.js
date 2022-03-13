979551d9288c9d612bebde48cd861f9e053db701
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-13 22:35:01 +0100

    CORS

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const config= require('config')
 const authRouter=require('./routes/auth.routes')
 const app = express()
 const PORT= config.get('serverPort')  
+const corsMiddleware= require('./middleware/cors.middleware')
 
+app.use(corsMiddleware)
 app.use(express.json())
 app.use('/api/auth', authRouter)
 
 const start = async () =>{
     try {
-     await mongoose.connect(config.get("dbUrl"))
+     await mongoose.connect(config.get("dbUrl"), {
+       userNewUrlParser: true,
+       userUnifiedTopology: true
+     })
         
         app.listen(PORT, ()=>{
             // console.log(`Server started on port`, PORT);
